42bf91ee68e0ffc0dd3f6fd1ec87b7f7ecd2f2e1
Carlos <carlos.av.amorim@gmail.com>
2022-01-17 22:50:09 -0300

    Page products finalizada

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -1,20 +1,43 @@
 import React from 'react'
 import { Container } from './style'
+import queijo from '../../img/queijos/capa.jpeg'
+import vinho from '../../img/vinhos/capa.jpg'
+import doce from '../../img/doces/capa.jpg'
+import { Link } from 'react-router-dom'
+
 
 export default function Products() {
   return (
     <Container>
+
       <section>
+
         <div>
-          Queijos
+          <img src={queijo} alt="queijos" />
+          <div className="texts">
+            <h1>Queijos</h1>
+            <Link className="links" to="/">Ver mais</Link>
+          </div>
         </div>
+
         <div>
-          Vinhos
+          <img src={vinho} alt="vinhos" />
+          <div className="texts">
+            <h1>Vinhos</h1>
+            <Link className="links" to="/">Ver mais</Link>
+          </div>
         </div>
+
         <div>
-          Doces
+          <img src={doce} alt="doces" />
+          <div className="texts">
+            <h1>Doces</h1>
+            <Link className="links" to="/">Ver mais</Link>
+          </div>
         </div>
+
       </section>
+
     </Container>
   )
 }
--- src/pages/product/style.js
+++ src/pages/product/style.js
@@ -18,5 +18,56 @@ export const Container = styled.main`
     justify-content: center;
     align-items: center;
 
+    div {
+      width: 30vw;
+      height: 60vh;
+      margin: 1vw;
+      justify-content: center;
+      align-items: center;
+      display: inline-block;
+      position: relative;
+
+      img {
+        height: 100%;
+        width: 100%;
+        background-repeat: no-repeat;
+        border-radius: 1rem;
+        filter: brightness(50%);
+      }
+      
+      .texts {
+        position: absolute;
+        top: 5rem;
+        right: 0;
+        left: -1vw;
+        text-align: center;
+        height: auto;
+        width: inherit;
+
+        h1 {
+          color: white;
+          font-size: 3rem;
+          margin-bottom: 2rem;
+        }
+
+        .links {
+          cursor: pointer;
+          padding: 0.5rem 2rem;
+          border-radius: 1rem;
+          border: 2px solid var(--white);
+          background-color: rgba(0,0,0, 0.2);
+          transition: all ease 0.2s;
+          color: var(--white);
+          font-size: 1.5rem;
+          font-weight: bold;
+          text-decoration: none;
+
+          &:hover {
+            background-color: rgba(0,0,0, 0.8);
+          }
+        }
+      }
+    }
   }
+  
 `
